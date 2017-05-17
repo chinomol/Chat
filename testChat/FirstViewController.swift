542cacda3d94f7b834ebf581c2c5112394aff3f1
Nicolás Molina <nicolasmolina@if-dcom.local>
2017-05-17 11:10:54 -0300

    Initial Commit

---
+++ testChat/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  testChat
+//
+//  Created by Nicolás Molina on 17/5/17.
+//  Copyright © 2017 IF-d. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
